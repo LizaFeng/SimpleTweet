1980e21523b58f7e049107138e2d89469e83b7ca
LizaFeng <62193040+LizaFeng@users.noreply.github.com>
2022-03-02 16:13:03 -0500

    adding the time stamp

--- app/src/main/java/com/codepath/apps/restclienttemplate/TweetsAdapter.kt
+++ app/src/main/java/com/codepath/apps/restclienttemplate/TweetsAdapter.kt
@@ -8,13 +8,81 @@ import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.codepath.apps.restclienttemplate.models.Tweet
+//The following imports are for the TimeStamp
+import java.text.ParseException
+import java.text.SimpleDateFormat
+import java.util.*
 
 
 //It needs to know what it is displaying (aka a list of tweets)
 //For each adapter, we need a viewholder to reference the actual views
 //changed list to array list so that we can use the clear and add all for pull to refresh
 class TweetsAdapter (val tweets: ArrayList<Tweet>): RecyclerView.Adapter<TweetsAdapter.ViewHolder>(){
+    //-----------------------------------------------------------------------
+    /**
+     * Given a date String of the format given by the Twitter API, returns a display-formatted
+     * String representing the relative time difference, e.g. "2m", "6d", "23 May", "1 Jan 14"
+     * depending on how great the time difference between now and the given date is.
+     * This, as of 2016-06-29, matches the behavior of the official Twitter app.
+     */
+    object TimeFormatter {
+        fun getTimeDifference(rawJsonDate: String?): String {
+            var time = ""
+            val twitterFormat = "EEE MMM dd HH:mm:ss ZZZZZ yyyy"
+            val format = SimpleDateFormat(twitterFormat, Locale.ENGLISH)
+            format.isLenient = true
+            try {
+                val diff = (System.currentTimeMillis() - format.parse(rawJsonDate).time) / 1000
+                if (diff < 5) time = "Just now" else if (diff < 60) time =
+                    String.format(Locale.ENGLISH, "%ds", diff) else if (diff < 60 * 60) time =
+                    String.format(
+                        Locale.ENGLISH, "%dm", diff / 60
+                    ) else if (diff < 60 * 60 * 24) time = String.format(
+                    Locale.ENGLISH, "%dh", diff / (60 * 60)
+                ) else if (diff < 60 * 60 * 24 * 30) time = String.format(
+                    Locale.ENGLISH, "%dd", diff / (60 * 60 * 24)
+                ) else {
+                    val now = Calendar.getInstance()
+                    val then = Calendar.getInstance()
+                    then.time = format.parse(rawJsonDate)
+                    time = if (now[Calendar.YEAR] == then[Calendar.YEAR]) {
+                        (then[Calendar.DAY_OF_MONTH].toString() + " "
+                                + then.getDisplayName(Calendar.MONTH, Calendar.SHORT, Locale.US))
+                    } else {
+                        (then[Calendar.DAY_OF_MONTH].toString() + " "
+                                + then.getDisplayName(Calendar.MONTH, Calendar.SHORT, Locale.US)
+                                + " " + (then[Calendar.YEAR] - 2000).toString())
+                    }
+                }
+            } catch (e: ParseException) {
+                e.printStackTrace()
+            }
+            return time
+        }
 
+        /**
+         * Given a date String of the format given by the Twitter API, returns a display-formatted
+         * String of the absolute date of the form "30 Jun 16".
+         * This, as of 2016-06-30, matches the behavior of the official Twitter app.
+         */
+        fun getTimeStamp(rawJsonDate: String?): String {
+            var time = ""
+            val twitterFormat = "EEE MMM dd HH:mm:ss ZZZZZ yyyy"
+            val format = SimpleDateFormat(twitterFormat, Locale.ENGLISH)
+            format.isLenient = true
+            try {
+                val then = Calendar.getInstance()
+                then.time = format.parse(rawJsonDate)
+                val date = then.time
+                val format1 = SimpleDateFormat("h:mm a \u00b7 dd MMM yy")
+                time = format1.format(date)
+            } catch (e: ParseException) {
+                e.printStackTrace()
+            }
+            return time
+        }
+    }
+    //-----------------------------------------------------------------------
     //Inflating the layout that we want to use for each of the items we want to display on the recyclerview
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TweetsAdapter.ViewHolder {
 
@@ -39,6 +107,8 @@ class TweetsAdapter (val tweets: ArrayList<Tweet>): RecyclerView.Adapter<TweetsA
         holder.tvUserName.text = tweet.user?.name
         //Setting the body of the tweet. The "body" is from Tweet.kt.
         holder.tvTweetBody.text = tweet.body
+        //Setting the time stamp for the tweet
+        holder.tvTimeStamp.setText(tweet.time_stamp)
 
         //The last thing we need for here is the profile image.
         //We will be using the Glide library which would makes image loading very easy for us.
@@ -74,6 +144,8 @@ class TweetsAdapter (val tweets: ArrayList<Tweet>): RecyclerView.Adapter<TweetsA
         val ivProfileImage = itemView.findViewById<ImageView>(R.id.ivProfileImage)
         val tvUserName = itemView.findViewById<TextView>(R.id.tvUsername)
         val tvTweetBody = itemView.findViewById<TextView>(R.id.tvTweetBody)
+        //For TimeStamp
+        val tvTimeStamp = itemView.findViewById<TextView>(R.id.tvTimeStamp)
 
 
     }
--- app/src/main/java/com/codepath/apps/restclienttemplate/models/Tweet.kt
+++ app/src/main/java/com/codepath/apps/restclienttemplate/models/Tweet.kt
@@ -1,5 +1,6 @@
 package com.codepath.apps.restclienttemplate.models
 
+import com.codepath.apps.restclienttemplate.TweetsAdapter
 import org.json.JSONArray
 import org.json.JSONObject
 
@@ -11,10 +12,12 @@ class Tweet {
     var createdAt: String = ""
     //We want to display who created the tweet.
     var user: User? = null
+    //For TimeStamp: We want to display the time stamp
+    var time_stamp: String = ""
 
     //Similar to User.kt, we want to have a companion object where we would have a function that actually allows us
         //to pass in a json object and turn that into a tweet object we can use
-    companion object{
+    companion object {
         //Creating a tweet object from a json object (parses one json object into a tweet)
         fun fromJson(jsonObject: JSONObject): Tweet {
             val tweet = Tweet()
@@ -22,20 +25,21 @@ class Tweet {
             tweet.body = jsonObject.getString("text")
             tweet.createdAt = jsonObject.getString("created_at")
             //Get the user class, utilize it from json method with the json object we will pass in
-                //The user is not just a string, its an actual User
+            //The user is not just a string, its an actual User
             tweet.user = User.fromJson(jsonObject.getJSONObject("user"))
+            //For TimeStamp
+            tweet.time_stamp = tweet.getFormattedTimestamp(tweet.createdAt)
             return tweet
-
 
         }
 
         //Method that takes a whole json array and convert that into a list of tweets
-        fun fromJsonArray(jsonArray: JSONArray): List<Tweet>{
+        fun fromJsonArray(jsonArray: JSONArray): List<Tweet> {
             val tweets = ArrayList<Tweet>()
             //We used .. before which meant inclusive (we went all the way to the end of the length of the
-                //json array but the length of the json array would be out of bounds (no index)
-                //until means exclusive
-            for(i in 0 until jsonArray.length()){
+            //json array but the length of the json array would be out of bounds (no index)
+            //until means exclusive
+            for (i in 0 until jsonArray.length()) {
                 tweets.add(fromJson(jsonArray.getJSONObject(i)))
             }
             return tweets
@@ -44,6 +48,9 @@ class Tweet {
         }
 
     }
+    //For TimeStamp
+    fun getFormattedTimestamp(string: String): String {
+        return TweetsAdapter.TimeFormatter.getTimeDifference(string)
 
-
+    }
 }
